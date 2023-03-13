35940b2d52abdcb7d382cb1dd95bfc0556b1b5ac
maya-tsedeke <32611349+maya-tsedeke@users.noreply.github.com>
2023-03-13 19:34:00 +0200

    Test

--- Integrify-s-Project/tests/WebUI.AcceptanceTests/GlobalUsings.cs
+++
@@ -1,5 +0,0 @@
-﻿global using BoDi;
-global using FluentAssertions;
-global using Integrify_s_Project.WebUI.AcceptanceTests.Pages;
-global using Microsoft.Playwright;
-global using TechTalk.SpecFlow;
